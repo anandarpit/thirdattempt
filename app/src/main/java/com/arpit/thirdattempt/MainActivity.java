520171d45ceb834d2327914b2f09a3820111e429
Arpit Anand <orionarpit@gmail.com>
2021-01-26 18:46:02 +0530

    ui

--- app/src/main/java/com/arpit/thirdattempt/MainActivity.java
+++ app/src/main/java/com/arpit/thirdattempt/MainActivity.java
@@ -4,21 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.amplifyframework.AmplifyException;
 import com.amplifyframework.api.aws.AWSApiPlugin;
 import com.amplifyframework.core.Amplify;
-import com.amplifyframework.core.model.query.Where;
 import com.amplifyframework.datastore.AWSDataStorePlugin;
+import com.amplifyframework.datastore.DataStoreException;
 import com.amplifyframework.datastore.generated.model.Priority;
 import com.amplifyframework.datastore.generated.model.Todo;
 
 public class MainActivity extends AppCompatActivity {
+EditText name, desription;
+RadioGroup priorityGroup;
+Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.name);
+        desription = findViewById(R.id.description);
+        priorityGroup = findViewById(R.id.radioGroup);
+        button = findViewById(R.id.submit);
+
         try {
             Amplify.addPlugin(new AWSDataStorePlugin());
             Amplify.addPlugin(new AWSApiPlugin());
@@ -28,54 +41,55 @@ public class MainActivity extends AppCompatActivity {
         } catch (AmplifyException e) {
             Log.e("Tutorial", "Could not initialize Amplify", e);
         }
-//        try {
-//            Amplify.addPlugin(new AWSDataStorePlugin());
-//            Amplify.configure(getApplicationContext());
-//
-//            Log.i("Tutorial", "Initialized Amplify");
-//        } catch (AmplifyException failure) {
-//            Log.e("Tutorial", "Could not initialize Amplify", failure);
-//        }
-//
+
+        button.setOnClickListener(view -> addtoDatastore());
+    }
+
+    private void addtoDatastore() {
+        if(name.getText().toString().isEmpty()){
+            name.setError("Name Cannot be empty");
+            Toast.makeText(MainActivity.this, "Name Cannot be empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
         Todo item = Todo.builder()
-                .name("fgtrtrtrddt")
-                .priority(Priority.HIGH)
-                .description("Thistrtrtrt kk")
+                .name(name.getText().toString())
+                .priority(getPriority())
+                .description(desription.getText().toString().trim())
                 .build();
         Amplify.DataStore.save(
                 item,
-                success -> Log.i("Tutorial", "Saved item: " + success.item().getName()),
-                error -> Log.e("Tutorial", "Could not save item to DataStore", error)
+                success -> onSuccess(),
+                this::onFailure
         );
 
-//        Amplify.DataStore.query(
-//                Todo.class,
-//                Where.matches(
-//                        Todo.PRIORITY.eq(Priority.HIGH)
-//                ),
-//                todos -> {
-//                    while (todos.hasNext()) {
-//                        Todo todo = todos.next();
-//
-//                        Log.i("Tutorial", "==== Todo ====");
-//                        Log.i("Tutorial", "Name: " + todo.getName());
-//
-//                        if (todo.getPriority() != null) {
-//                            Log.i("Tutorial", "Priority: " + todo.getPriority().toString());
-//                        }
-//
-//                        if (todo.getDescription() != null) {
-//                            Log.i("Tutorial", "Description: " + todo.getDescription());
-//                        }
-//                    }
-//                },
-//                failure -> Log.e("Tutorial", "Could not query DataStore", failure)
-//        );
-//        Amplify.DataStore.observe(Todo.class,
-//                started -> Log.i("Tutorial", "Observation began."),
-//                change -> Log.i("Tutorial", change.item().toString()),
-//                failure -> Log.e("Tutorial", "Observation failed.", failure),
-//                () -> Log.i("Tutorial", "Observation complete.")
-//        );
+    }
+
+    private void onFailure(DataStoreException e) {
+        Log.e("Tutorial", "Could not save item to DataStore", e);
+    }
+
+    private void onSuccess() {
+        Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show();
+        name.setText("");
+        desription.setText("");
+        priorityGroup.clearCheck();
+    }
+
+    private Priority getPriority() {
+        if(priorityGroup.getCheckedRadioButtonId() == -1){
+            return null;
+        }
+        else{
+            switch (priorityGroup.getCheckedRadioButtonId()) {
+                case 0:
+                    return  Priority.LOW;
+                case 1:
+                    return  Priority.NORMAL;
+                case 2:
+                    return  Priority.HIGH;
+                default:
+                    return null;
+            }
+        }
     }
 }
